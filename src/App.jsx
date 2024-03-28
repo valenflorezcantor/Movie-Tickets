8cf0f7cd53f00973f846e86e0726c152776a641e
Valentina Florez <valenflorca@gmail.com>
2024-03-27 21:54:10 -0500

    consuming application TMDB

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./routing/Layout/Layout";
 
-// import Genre from "./Genre";
-// import NotFound from "./NotFound";
+import { useEffect,useState } from "react";
+import { getAllMovies } from "./services/movieServices";
+
+
+
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    getAllMovies().then((response)=>{
+      console.log(response);
+      setMovies(response);
+    });
+  }, []);
+  
+  
   return (
     <BrowserRouter>
       <Routes>
